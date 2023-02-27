45779e672a1d86d85369efaff09d25593266cea4
nick <56604584+Nico7777777@users.noreply.github.com>
2023-02-27 16:58:58 +0200

    Pandas approach - optimized

--- main.py
+++ main.py
@@ -1,57 +1,73 @@
+from sklearn.linear_model import LogisticRegression
 from numpy import random
+import numpy as np
+import pandas as pd
 import csv
 
+columns = ['Sex', 'Parch', 'Age', 'Embarked']
 answerfile = "answer.csv"
-filename = "test.csv"
-new_column = "Survived"
-age = 0
+train = "train.csv"
+test = "test.csv"
 
 
-def sex_rate(arg):
-    SVSR = [0.12, 0.26]  # sex values survive rates
-    if arg == 'male':
-        return SVSR[0]
-    else:
-        return SVSR[1]
+def sex(arg):
+    return 1 if arg == 'male' else 0
 
-def age_rate(arg):
-    AVSR = [0.02, 0.075, 0.15, 0.059, 0.059] # age values survive rates
+
+def age(the_age):
+    avr = [0.07, 0.25, 0.47, 0.21]  # age values survive rates
 
     try:
-        age = float(line[4])
+        the_age = float(the_age)
     except ValueError:
-        age = 100000
-    if age >= 0 and age <= 8:
-       return AVSR[0]
-    elif age > 8 and age <= 23:
-        return AVSR[1]
-    elif age > 23 and age <= 41:
-        return AVSR[2]
-    elif age > 41 and age <= 80:
-        return AVSR[3]
+        the_age = 100000
+    if 0 <= the_age <= 8:
+        return 1
+    elif 8 < the_age <= 23:
+        return 2
+    elif 23 < the_age <= 41:
+        return 3
+    elif 41 < the_age <= 80:
+        return 4
     else:
-        return AVSR[4]
+        return random.choice([1, 2, 3, 4], p=avr)
 
-def preddiction(sex_arg, age_arg):
-    survive_percentage = (age_rate(age_arg) + sex_rate(sex_arg))/2
-    return random.choice([1, 0], 1, p=[survive_percentage, 1-survive_percentage])[0]
-    
+
+def checkpoint(arg):
+    if arg == 'Q':
+        return 1
+    elif arg == 'S':
+        return 2
+    else:
+        return 3
+
+
+def advanced_preddiction():
+    train_ds = pd.read_csv("train.csv", usecols=['Embarked', 'Age', 'Sex', 'Parch', 'Survived'])
+    train_ds['Embarked'] = [checkpoint(arg) for arg in train_ds['Embarked']]
+    train_ds['Age'] = [age(arg) for arg in train_ds['Age']]
+    train_ds['Sex'] = [sex(arg) for arg in train_ds['Sex']]
+    features = np.array([train_ds['Age'], train_ds['Parch'], train_ds['Sex'], train_ds['Embarked']]).swapaxes(0, 1)
+
+    model = LogisticRegression()
+    y = np.array(train_ds['Survived'])
+    model.fit(features, y)
+
+    test_ds = pd.read_csv("test.csv", usecols=['Embarked', 'Age', 'Sex', 'Parch', 'PassengerId'])
+    test_ds['Embarked'] = [checkpoint(arg) for arg in test_ds['Embarked']]
+    test_ds['Age'] = [age(arg) for arg in test_ds['Age']]
+    test_ds['Sex'] = [sex(arg) for arg in test_ds['Sex']]
+    features = np.array([test_ds['Age'], test_ds['Parch'], test_ds['Sex'], test_ds['Embarked']]).swapaxes(0, 1)
+    pred = model.predict(features)
+    print(pred[0])
+
+    with open("answer_sheet.csv", 'w') as f:
+        results = csv.writer(f, lineterminator='\n')
+        results.writerow(["PassengerId", "Survived"])
+        for i in range(418):
+            results.writerow([test_ds['PassengerId'][i], pred[i]])
+
+
 if __name__ == "__main__":
     random.seed(0)
-    with open(filename, 'r') as f:
-        with open(answerfile, 'w') as g:
-            test = csv.writer(g, lineterminator='\n')
-            answer = csv.reader(f)
-
-            all = [] # matricea cu raspunsuri
-            line = next(answer)[:1] # coloana cu PassengerID-ul
-            line.append(new_column) # bagam a coloana cu Survive
-            all.append(line) # header-ul
-
-            for line in answer:
-                preddict = preddiction(line[3], line[4])
-                line = line[:1]
-                line.append(preddict) #aici trebuie sa inlocuiesc
-                all.append(line)
-
-            test.writerows(all)
+    advanced_preddiction()
